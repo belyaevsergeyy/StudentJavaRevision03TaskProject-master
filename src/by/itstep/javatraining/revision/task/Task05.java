4e91d8442e4de1a81d9165900ce4cbc7594b6910
SergeyBelyaev <belyaevsergeyy@rambler.ru>
2023-04-09 14:42:14 +0300

    Task 05 well done

--- src/by/itstep/javatraining/revision/task/Task05.java
+++ src/by/itstep/javatraining/revision/task/Task05.java
@@ -23,6 +23,9 @@ package by.itstep.javatraining.revision.task;
 
 public class Task05 {
     public static boolean task05(int x1, int y1, int x2, int y2) {
+        if (y1 > 0 && y1 < 9 && y2 > 0 && y2 < 9 && x1 > 0 && x1 < 9 && x2 > 0 && x2 < 9) {
+            return x1 != x2 && (x2 - y2 == x1 - y1 || x2 - x1 == -(y2 - y1));
+        }
         return false;
     }
 }
